6e9af3b65907bfaa030f702ea2f24817e62a3241
Minsu Park <0601p@naver.com>
2021-11-10 10:53:51 +0900

    btn1_widget update
    date

--- btn1_widget.py
+++ btn1_widget.py
@@ -4,5 +4,9 @@ widget1_class = uic.loadUiType("btn1_widget.ui")[0]
 
 class Ui_btn1_widget(widget1_class, QtWidgets.QWidget):
     def change_week(self, week):
-        for day in week:
-            print(day.day)
+        date = ''
+        for day in week[:6]:
+            date += str(day.month)+ '.' + str(day.day) + '   '
+        date += str(week[6].month) + '.' + str(week[6].day)
+        self.sleep_range.setText(date)
+        self.active_range.setText(date)
